d12cbb7981af558202547f2e4664ca38f8a974f2
Orlando Junior de Souza Navarro <42560384+OrlandoNavarro@users.noreply.github.com>
2025-04-02 06:41:00 -0300

    Update roterizador.py

--- roterizador.py
+++ roterizador.py
@@ -387,7 +387,7 @@ def criar_mapa(pedidos_df):
     
     # Adicionar marcadores para os pedidos
     for _, row in pedidos_df.iterrows():
-        popup_text = f"Placa: {row['Placa']}<br><b>Endereço: {row['Endereço Completo']}</b>"
+        popup_text = f"Placa: {row['Placa']}</b><br><b>Endereço: {row['Endereço Completo']}"
         folium.Marker(
             location=[row['Latitude'], row['Longitude']],
             popup=popup_text,
